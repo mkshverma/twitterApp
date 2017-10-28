1e7a72b54a047d14d9e2575b407c75269150d56d
mkshverma <mkshverma560@gmail.com>
2017-10-28 18:15:03 +0530

    fix errrrs

--- api.php
+++ api.php
@@ -2,7 +2,7 @@
 session_start();
 header('Access-Control-Allow-Origin: *');
 define('BASE_PATH','C:/xampp/htdocs/www/twitterapp/');
-define('BASE_URL','http://localhost:8888/');
+define('BASE_URL','http://35.202.132.52/');
 require('src/fetchData.php');
 $tw = new TwitterApp();
 $function = @$_GET['call'];
