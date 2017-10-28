fe2c5cae5e7f432b2543358bb703089c3ca862c8
mkshverma <mkshverma560@gmail.com>
2017-10-28 19:34:12 +0530

    asda test

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 header('Access-Control-Allow-Origin: *');
-define('BASE_PATH','C:/xampp/htdocs/www/twitterapp/');
+define('BASE_PATH','/opt/bitnami/apache2/htdocs/');
 define('BASE_URL','http://35.202.132.52/');
 require('src/fetchData.php');
 $tw = new TwitterApp();
